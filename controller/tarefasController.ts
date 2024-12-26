21968294b1e2a9212dcf367b801d0d9b8cb617cb
christian <christiandeveloper123@gmail.com>
2024-12-26 16:20:53 -0300

    Criaçäo do controller

---
+++ controller/tarefasController.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient()
+
+const criarTarefa = async (req, res) => {
+    try{
+        const {nome, descricao} = req.body
+        const newTarefa = await prisma.tarefas.create({
+            data: {
+                name : nome,
+             description: descricao
+            },
+        });
+        res.status(200).json({newTarefa})
+    } catch(error){
+        res.status(500).json({ error: "Deu Ruim"})
+    }
+}
