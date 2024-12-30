4093ef5f632fefc70f0f66f790343316ca188293
christian <christiandeveloper123@gmail.com>
2024-12-30 17:58:10 -0300

    Criação da função que lista todas as tarefas do db

--- controller/tarefasController.js
+++ controller/tarefasController.js
@@ -3,7 +3,6 @@ const prisma = new PrismaClient()
 
 const criarTarefa = async (req, res) => {
     try{
-        console.log("corpo", req.body)
         const {nome, descricao} = req.body
 
         if (!nome || !descricao) {
@@ -23,4 +22,14 @@ const criarTarefa = async (req, res) => {
     }
 }
 
-module.exports = { criarTarefa };
+const listarTarefas = async (req, res) => {
+    try {
+        const TodasTarefas = await prisma.tarefas.findMany()
+        res.status(200).json({ TodasTarefas })
+    } catch (error){
+        res.status(500).json({ error: "Erro ao encontrar as tarefas", error })
+    }
+    
+}
+
+module.exports = { criarTarefa, listarTarefas };
--- routes/tarefaRouter.js
+++ routes/tarefaRouter.js
@@ -3,5 +3,6 @@ const TarefaController = require("../controller/tarefasController");
 const router = express.Router();
 
 router.post('/tarefas', TarefaController.criarTarefa);
+router.get('/tarefas', TarefaController.listarTarefas)
 
 module.exports = router;
